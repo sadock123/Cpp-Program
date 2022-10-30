224955dfc38a0faafc7662cd40d19cdebce6c1b5
Samir Giripunje <samirgiripunje5@gmail.com>
2022-10-30 19:32:56 +0530

    Contest 831 problem A added

---
+++ Codeforces/Contest 831/Problem_A_factorise_N_M.cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define all(x)     (x).begin(), (x).end()
+#define TC(x)          sci(x); while(x --)
+#define print(v)     for(int i=0;i<v.size();i++)
+#define endl        "\n"
+ 
+int main(){
+ios::sync_with_stdio(false);
+int t;
+cin>>t;
+while(t--)
+{
+    int n;
+    cin>>n;
+    if(n==2)
+        cout<<7<<endl;
+    else
+      cout<<3<<endl;
+ 
+ 
+}
+ 
+ 
+return 0;
+}
